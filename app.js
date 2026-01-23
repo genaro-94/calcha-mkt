b931d7bdd01f416612e69cc091e18f386130e180
genaro-94 <genaritono14@gmail.com>
2026-01-23 05:05:54 -0300

    Update app.js

--- app.js
+++ app.js
@@ -133,7 +133,7 @@ function renderHome() {
   </button>
 
   <button class="accion-btn" data-rubro="motodelivery">
-    🛵📦 Delivery
+    🛵📦 Delivery/Paqueteria
   </button>
 </section>
   <section class="rubros-grid">
